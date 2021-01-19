fc2b16c0401ea97e9f4c61e3aa59d59fa668da22
PawelBudniak <pawel.b714@gmail.com>
2021-01-20 00:25:22 +0100

    test n3 generator

--- testMain.cpp
+++ testMain.cpp
@@ -18,6 +18,7 @@
 #include <numeric>
 #include <algorithm>
 #include <chrono>
+#include <windows.h>
 
 void combinations(int offset, int k, int nVert, std::vector<int> & curr, std::vector<std::vector<int>> & result) {
     if (k == 0) {
@@ -31,9 +32,19 @@ void combinations(int offset, int k, int nVert, std::vector<int> & curr, std::ve
     }
 }
 
-void getGraph(int nVert = 10){
+Graph<LinkedVertex, LinkedVertexList> getGraph(int nVert = 10){
     using namespace boost;
     typedef adjacency_list<vecS, vecS, undirectedS> MyGraphType;
+
+
+
+    std::mt19937_64 rng;
+    // initialize the random number generator with time-dependent seed
+    uint64_t timeSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    std::seed_seq ss{uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed>>32)};
+    rng.seed(ss);
+    // initialize a uniform distribution between 0 and 1
+    std::uniform_real_distribution<double> unif(0, 1);
 
     MyGraphType g;
 
@@ -43,25 +54,27 @@ void getGraph(int nVert = 10){
     std::vector<std::vector<int>> possible_edges;
     combinations(0,2,nVert, temp,possible_edges);
 
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::shuffle(possible_edges.begin(), possible_edges.end(), std::default_random_engine(seed));
+    std::shuffle(possible_edges.begin(), possible_edges.end(), std::default_random_engine(timeSeed));
 
 
     for (const auto & edge: possible_edges){
-        add_edge(edge[0], edge[1], g);
-        if (!boyer_myrvold_planarity_test(g))
-            remove_edge(edge[0],edge[1],g);
+        //if (unif(rng) >  0.2) {
+            add_edge(edge[0], edge[1], g);
+            if (!boyer_myrvold_planarity_test(g))
+                remove_edge(edge[0], edge[1], g);
+        //}
     }
 
+    Graph<LinkedVertex, LinkedVertexList> myg(nVert);
 
     auto t = edges(g);
     for (auto& it = t.first; it != t.second; ++it){
         int u = source(*it,g);
-        auto v = source(*it,g);
-        std::cout << *it << std::endl;
-
+        int v = target(*it,g);
+        //std::cout <<"to: " <<  target(*it,g)  << " " << source(*it,g) << std::endl;
+        myg.addEdge(u,v);
     }
-
+    return myg;
 
 
 }
@@ -87,33 +100,61 @@ int main(int argc, char** argv)
 //        std:: cout << std:: endl;
 //    }
 
+//
+//    auto time = std::chrono::system_clock::now().time_since_epoch();
+//    Graph<LinkedVertex, LinkedVertexList> g = getGraph(10);
+//    std::cout << g;
+//    auto diff = std::chrono::system_clock::now().time_since_epoch() - time;
+//    std::cout << "ms: " << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
 
-    getGraph(100);
+    int step = 10;
+    int max = 70;
+    int start = 10;
+    int maxGraphs = 30;
 
-//    int step = 5;
-//    int max = 40;
-//    int start = 15;
-//    int maxGraphs = 30;
-//
-//
-//    for (int n_vert = start; n_vert <= max; n_vert += step) {
-//        Generator::generate(n_vert, maxGraphs);
-//
-//        std::ifstream fp(Generator::OUTPUT_FILE);
-//        std::string line;
-//        while (std::getline(fp, line)) {
-//            Graph<LinkedVertex, LinkedVertexList> g;
-//            g.fromAscii(line);
-//            testAllAlgorithmsCorrectness(g);
+    std::vector<double> times;
+
+    for (int n_vert = start; n_vert <= max; n_vert += step) {
+        //Generator::generate(n_vert, maxGraphs);
+
+        //Sleep(10 * 1000);
+
+
+        double avg = 0;
+
+        //std::ifstream fp(Generator::OUTPUT_FILE);
+        //std::string line;
+        //while (std::getline(fp, line)) {
+        for (int i = 0; i < maxGraphs; ++i) {
+            Graph<LinkedVertex, LinkedVertexList> g = getGraph(n_vert);
+            //g.fromAscii(line);
+
+            //testAllAlgorithmsCorrectness(g);
+            auto time = std::chrono::steady_clock::now().time_since_epoch();
+            colorLinear5(g);
+            auto diff = std::chrono::steady_clock::now().time_since_epoch() - time;
+
+            avg += std::chrono::duration_cast<std::chrono::microseconds>(diff).count();
+
+
 //            std::cout << g << std::endl;
 //            std::cout << "################################### " << std::endl;
-//        }
-//    }
+            //}
+        }
+
+        times.push_back(avg/maxGraphs);
+
+    }
+    std::cout << "times: new york times taka gazeta" << std::endl;
+
+    for (auto& v: times){
+        std::cout << v << " ";
+    }
+
+    //remove(Generator::OUTPUT_FILE.c_str());
 //
-//    remove(Generator::OUTPUT_FILE.c_str());
-
-
-    return 0;
+//
+//    return 0;
 }
 
 void manualMode(Graph<int, std::set<int>>& graph)
